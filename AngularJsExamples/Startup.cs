560d4a67294ac62c45537ca65ba62d8af3780a52
srikanth <jayavarapu@outlook.com>
2016-10-26 20:13:25 +0530

    Initial project + basic custom directives are created

---
+++ AngularJsExamples/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AngularJsExamples.Startup))]
+namespace AngularJsExamples
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
---
+++ AngularJsExamples/app/01CustomDirective/app.js
@@ -0,0 +1,20 @@
+﻿/// <reference path="C:\vs2015\AngularJsExamples\AngularJsExamples\Scripts/angular.js" />
+
+var app= angular.module('app', []);
+
+app.controller('empController', ['$scope', function($scope){
+    $scope.name = "this is basic Directive";
+    $scope.firstname = "srikanth";
+}]);
+
+app.directive('myInfoMsg', function () {
+    return {
+        template: "<strong>{{name}}</strong>"
+    };
+})
+
+app.directive('mySecondMsg', function () {
+    return {
+        templateUrl: "my-second-msg.html1"
+    };
+});
